5dd48d47f270925664604443751ef7af77753722
Suryashivaji <devilsurya265@gmail.com>
2024-04-01 11:04:03 +0530

    mongoose

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
 import express from 'express'
 import AppRoutes from './routes/index.js'
 import dotenv from 'dotenv'
+import cors from 'cors'
 dotenv.config()
 const app = express()
 let PORT = process.env.PORT 
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/',AppRoutes)
